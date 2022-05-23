ff61f33f26c4dfdb66f0cf351e539c94514b9906
ahmed-razic <ahmed.razic@gmail.com>
2022-05-23 16:07:39 +0200

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log('Hello, World!');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorHandler');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server connected on port ${port}.`);
+});
