a885ca7620fb91cdb9aa22869f80448cb715cac3
ahmed-razic <ahmed.razic@gmail.com>
2022-05-23 15:06:10 +0200

    initial commit

---
+++ backend/middleware/errorHandler.js
@@ -0,0 +1,12 @@
+const errorHandler = (req, res, err, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENVIROMENT === 'production' ? null : err.stack,
+  });
+};
+
+module.exports = {
+  errorHandler,
+};
