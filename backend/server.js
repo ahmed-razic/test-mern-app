1445f253b7036df4b6de8c95e3ca90ecdce6da04
ahmed-razic <ahmed.razic@gmail.com>
2022-05-24 13:43:12 +0200

    connected to mongoodb

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const port = process.env.PORT;
+const MongoDB = require('./database/db');
 
 const app = express();
+MongoDB();
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}.`);
